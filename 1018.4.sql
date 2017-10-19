471cdd3067862bd4356ff0d9d311b415e44b3b86
s16001 <s16001@std.it-college.ac.jp>
2017-10-19 14:21:47 +0900

    修正再提出

--- 1018.4.sql
+++ 1018.4.sql
@@ -1,4 +1,4 @@
-select e.last_name, e.department_id, job_id
-from employees e, locations l
-where e.department_id in (select e.department_id from employees where l.location_id = 1700)
+select e.last_name, e.department_id, e.job_id
+from employees e, departments d 
+where d.department_id in (select e.department_id from employees where d.location_id = 1700)
 /
