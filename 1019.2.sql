7571f690642713770089efcbf40b939716004db5
s16001 <s16001@std.it-college.ac.jp>
2017-10-24 14:36:30 +0900

    修正

--- 1019.2.sql
+++ 1019.2.sql
@@ -1,4 +1,7 @@
-select department_id from employees where department_id is not null
-intersect
-select department_id from employees where job_id != 'ST_CLERK'
+select department_id
+from departments
+minus
+select distinct department_id
+from employees
+where job_id = 'ST_CLERK'
 /
