28f43d6e1c5ee04a1a66388d0127787acff56edd
s16001 <s16001@std.it-college.ac.jp>
2017-11-02 14:11:40 +0900

    修正

--- 1101.2.sql
+++ 1101.2.sql
@@ -1,4 +1,5 @@
-select last_name, job_id, salary, trunc(salary * 1.10), commission_pct from employees
+select last_name, job_id, salary, trunc(salary * 1.10) from employees
 where commission_pct is null
 order by salary desc
+fetch first 5 percent rows only
 /
