7ee158e7ca13176adf34119587abfdb83d48419c
s16001 <s16001@std.it-college.ac.jp>
2017-11-29 13:42:46 +0900

    再提出

--- 1128/1128.2.sql
+++ 1128/1128.2.sql
@@ -1,4 +1,4 @@
-select employee_id, last_name, round((sysdate - hire_date) / 12) as "TENURE"
+select employee_id, last_name, round(months_between(sysdate, hire_date)) as "TENURE"
 from employees
 where department_id = 90
 order by employee_id
